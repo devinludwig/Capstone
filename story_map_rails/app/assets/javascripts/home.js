a47080afceb0bf0f62404283880f4ee33d2f4aff
Devin Ludwig <devinludwig@gmail.com>
2017-03-06 12:26:09 -0800

    push all temperature anomaly values from data into an array in order to scale visualizations

--- story_map_rails/app/assets/javascripts/home.js
+++ story_map_rails/app/assets/javascripts/home.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
+  var width = 420,
+  barHeight = 20;
+
+  var x = d3.scaleLinear()
+      .range([0, width]);
+
+  var chart = d3.select("#visualization")
+      .attr("width", width);
+
   d3.tsv("data/IPCC AR4 multi-model average of detrended globally averaged TAS anomalies relative to 1980-1999 - Sheet2.tsv", function(error, data) {
     console.log(data);
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.close = +d.close;
+  var values = [];
+  debugger;
+  data.forEach(function(d) {
+    Object.values(d).forEach(function(v){
+      if (v > -20 && v < 20) {
+        values.push(v);
+      };
+    });
+  });
+  console.log(values)
+
+  x.domain([d3.min(values), d3.max(data)]);
+  data.forEach(function(d) {
     });
   });
 
