4d45b643841fbd9d3106ec0e4fb2174c202fd35e
Devin Ludwig <devinludwig@gmail.com>
2017-03-06 18:48:30 -0800

    start a stream graph that displays something but needs to be tweaked because of filler values in the data

--- story_map_rails/app/assets/javascripts/home.js
+++ story_map_rails/app/assets/javascripts/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var width = 420,
+  var width = 2000,
   barHeight = 20;
 
   var x = d3.scaleLinear()
@@ -17,18 +17,26 @@ $(document).ready(function(){
       d["B1"] = +d["B1"];
       d["A1B"] = +d["A1B"];
       d["A2"] = +d["A2"];
+
       Object.values(d).forEach(function(v){
         if (v > -20 && v < 20) {
           values.push(v);
-        };
+        }
       });
     });
-    console.log(data[0])
-    x.domain(d3.extent(values));
-    console.log(x.domain());
-    data.forEach(function(d) {
-      });
-  });
+      x.domain(d3.extent(values));
+      chart.attr("height", barHeight * data.length);
+      var bar = chart.selectAll("g").data(data).enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-  d3.select("#visualization").html("Hello, world!");
+      bar.append("rect")
+          .attr("width", function(d) { return x(d["commit"]); })
+          .attr("height", barHeight - 1);
+
+      bar.append("text")
+          .attr("x", function(d) { return x(d["commit"]) - 3; })
+          .attr("y", barHeight / 2)
+          .attr("dy", ".35em")
+          .text(function(d) { return d["commit"]; });
+  });
 });
---
+++ story_map_rails/app/assets/javascripts/stream.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  var width = 960,
+      height = 500;
+  var x = d3.scaleLinear().range([0, width]);
+  var y = d3.scaleLinear().range([height, 0]);
+  var xAxis = d3.axisBottom(x);
+  var color = d3.scaleLinear().range(["#aad", "#556"]);
+  var stream = d3.select("#stream").attr("width", width).attr("height", height);
+
+
+  d3.tsv("data/IPCC AR4 multi-model average of detrended globally averaged TAS anomalies relative to 1980-1999 - Sheet2.tsv", function(error, data) {
+    var values = [];
+    data.forEach(function(d) {
+      d["year"] = +d["year"];
+      d["20C3M"] = +d["20C3M"];
+      d["commit"] = +d["commit"];
+      d["B1"] = +d["B1"];
+      d["A1B"] = +d["A1B"];
+      d["A2"] = +d["A2"];
+
+      Object.values(d).forEach(function(v){
+        if (v > -20 && v < 20) {
+          values.push(v);
+        }
+      });
+    });
+
+    var layers = Object.values(data[0]).length - 1;
+    var years = data.length;
+    var  stack = d3.stack()
+        .keys(['20C3M',	'commit',	'B1',	'A1B',	'A2'])
+  			.offset(d3.stackOffsetWiggle);
+    var series = stack(data);
+    x.domain([data[0].year, data[data.length -1].year]);
+    y.domain([-5, 5]);
+
+
+    console.log(x.domain() + " " + y.domain());
+
+    var area = d3.area()
+        .x(function(d, i) { return x(d.data.year); })
+        .y0(function(d) { return y(d[0]); })
+        .y1(function(d) { return y(d[1]); })
+    		.curve(d3.curveBasis);
+debugger;
+    stream.selectAll("path").data(series).enter().append("path").attr("d", area).style("fill", function() { return color(Math.random()); });
+    stream.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + (height) + ")")
+            .call(xAxis);
+  });
+});
